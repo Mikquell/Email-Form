f0ea058264943c37168c28b6c1cab72cb29ef27c
Mikquell <angellomchl@gmail.com>
2024-04-05 14:05:30 +0700

    Initial commit

---
+++ app/src/main/java/com/michaelaor/emailform/MainActivity.java
@@ -0,0 +1,47 @@
+package com.michaelaor.emailform;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button _kirimbutton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initKirimButton();
+    }
+
+    private void initKirimButton() {
+        _kirimbutton = findViewById(R.id.kirimButton);
+
+        _kirimbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NotificationChannel channel = new NotificationChannel("twChannel","TW", NotificationManager.IMPORTANCE_DEFAULT);
+
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext())
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle("E-mail Form")
+                        .setContentText("E-mail sudah dikirim kepada anda")
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                        .setChannelId(channel.getId());
+
+                NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                manager.createNotificationChannel(channel);
+                manager.notify(1, builder.build());
+            }
+        }); {
+
+        };
+    }
+}
